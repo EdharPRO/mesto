c5d1a56263ab7b79731827487e208b5d2ef1ab1e
Edhar Prokudin <prokudin.ed@gmail.com>
2020-10-12 02:01:07 +0300

    initi commit

--- script/index.js
+++ script/index.js
@@ -1,44 +1,30 @@
-const PopAddBtn = document.querySelector('.profile__Edit-button')
-const Popprof = document.querySelector('.popup');
-const PopBtnClose = document.querySelectorAll('.popup__close');
-const PopForm = document.querySelector('.popup__form');
+let Popprof = document.querySelector('.popup');
+let PopAddBtn = document.querySelector('.profile__Edit-button');
+let ProfInfoName = document.querySelector('.profile__info-name');
+let PopName = Popprof.querySelector('.popup__name');
+let PopText = Popprof.querySelector('.popup__text');
+let PopInfoText = document.querySelector('.profile__info-text');
+let PopForm = document.querySelector('.popup__form');
+let PopBtnSave = Popprof.querySelector('.popup__save');
+let PopBtnClose = Popprof.querySelector('.popup__close');
 
-const ProfInfoName = document.querySelector('.profile__info-name');
-const PopInfoText = document.querySelector('.profile__info-text');
-const PopName = document.querySelector('.popup__name');
-const PopText = document.querySelector('.popup__text');
-
-
-const PopBtnSave = document.querySelector('.popup__save');
-
-
-const openPopup = mod => {
-  mod.classList.add('popup_open');
+function openPopup() {
+PopName.value = ProfInfoName.textContent;
+PopText.value = PopInfoText.textContent;
+Popprof.classList.add('popup_open');
 }
 
-
-PopAddBtn.addEventListener('click', () => {
-  openPopup(Popprof);
-});
-
-
-PopBtnClose.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    closePopup(event.target.closest('.popup'));
-  });
-});
-
-function handleFormSubmit (evt) { 
-    evt.preventDefault(); 
-    ProfInfoName.textContent = PopName.value; 
-    PopInfoText.textContent = PopText.value; 
-    closePopup(evt); 
-    PopName.value ='';
-    PopText.value ='';    
-} 
-
-PopForm.addEventListener('submit', handleFormSubmit);
-
-const closePopup = mod => {
-  mod.classList.remove('popup_open');
+function closePopup() {
+Popprof.classList.remove('popup_open');
 }
+
+function handlePopupSubmit(event) {
+event.preventDefault();
+ProfInfoName.textContent = PopName.value;
+PopInfoText.textContent = PopText.value;
+closePopup();
+}
+
+PopAddBtn.addEventListener('click', openPopup);
+PopBtnClose.addEventListener('click', closePopup);
+PopForm.addEventListener('submit', handlePopupSubmit);
